beb371180f6ff61f6b770a640583bbd0f43a094a
mktcode <kontakt@markus-kottlaender.de>
2017-05-24 19:29:37 +0200

    added notifications and display settings

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -33,6 +33,36 @@ $(function () {
         }
     });
 
+    // toggle ratios display
+    $(document).on('click', '.toggle-ratio', function () {
+        $(this).parents('.watch').find('.ratio-' + $(this).data('ratio')).toggleClass('uk-hidden');
+        $(this).parents('li').toggleClass('uk-active');
+    });
+
+    // toggle ratios display
+    var notifications = {};
+    $(document).on('click', '.toggle-notification', function () {
+        var pair = $(this).parents('.watch').data('pair'),
+            ratio = $(this).data('ratio');
+
+        if (pair in notifications) {
+            if ('n' + ratio in notifications[pair]) {
+                notifications[pair]['n' + ratio] = notifications[pair]['n' + ratio] ? 0 : 1;
+            } else {
+                notifications[pair]['n' + ratio] = 1;
+            }
+        } else {
+            notifications[pair] = {};
+            notifications[pair]['n' + ratio] = 1;
+        }
+
+        if (notifications[pair]['n' + ratio] == 1 && !Push.Permission.has()) {
+            Push.Permission.request();
+        }
+
+        $(this).parents('li').toggleClass('uk-active');
+    });
+
     // remove watch
     $(document).on('click', '.watch-close', function (e) {
         e.preventDefault();
@@ -47,79 +77,209 @@ $(function () {
     });
 
     // update values
+    var interval = 5000,
+        ratioHistory = {};
     setInterval(function () {
-        var time = Math.round(new Date().getTime() / 1000);
+        var now = new Date(),
+            time = Math.round(new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds()).getTime() / 1000) + 7200;
         $('.watch').each(function (i, item) {
             var watch = $(item),
                 pair = watch.data('pair');
 
             $.get(
                 'https://poloniex.com/public',
-                {command: 'returnTradeHistory', currencyPair: pair, start: time - 300, end: time},
+                {command: 'returnTradeHistory', currencyPair: pair, start: time - 3600, end: time},
                 function(data) {
-                    var buy = 0,
-                        sell = 0;
+                    var buy1 = 0,
+                        sell1 = 0,
+                        buy5 = 0,
+                        sell5 = 0,
+                        buy15 = 0,
+                        sell15 = 0,
+                        buy30 = 0,
+                        sell30 = 0,
+                        buy60 = 0,
+                        sell60 = 0;
 
                     $.each(data, function (i, trade) {
+                        var tradeTime = Math.round(new Date(trade.date).getTime() / 1000) + 7200,
+                            tradeAmount = parseFloat(trade.amount);
+
                         if (trade.type == 'buy') {
-                            buy = buy + parseFloat(trade.amount);
+                            if (time - tradeTime < 3600) {
+                                buy60 = buy60 + tradeAmount;
+                            }
+                            if (time - tradeTime < 1800) {
+                                buy30 = buy30 + tradeAmount;
+                            }
+                            if (time - tradeTime < 900) {
+                                buy15 = buy15 + tradeAmount;
+                            }
+                            if (time - tradeTime < 300) {
+                                buy5 = buy5 + tradeAmount;
+                            }
+                            if (time - tradeTime < 60) {
+                                buy1 = buy1 + tradeAmount;
+                            }
                         } else if (trade.type == 'sell') {
-                            sell = sell + parseFloat(trade.amount);
+                            if (time - tradeTime < 3600) {
+                                sell60 = sell60 + tradeAmount;
+                            }
+                            if (time - tradeTime < 1800) {
+                                sell30 = sell30 + tradeAmount;
+                            }
+                            if (time - tradeTime < 900) {
+                                sell15 = sell15 + tradeAmount;
+                            }
+                            if (time - tradeTime < 300) {
+                                sell5 = sell5 + tradeAmount;
+                            }
+                            if (time - tradeTime < 60) {
+                                sell1 = sell1 + tradeAmount;
+                            }
                         }
                     });
 
-                    // set indicator
+                    // set indicator (based on 5 min ratio)
                     var ratioHeight = 1;
-                    if (buy > sell) {
-                        ratioHeight = (sell / buy).toFixed(2);
+                    if (buy1 > sell1) {
+                        ratioHeight = (sell1 / buy1).toFixed(2);
                         watch.find('.indicator').find('.down').css('height', '0');
                         watch.find('.indicator').find('.up').css('height', Math.floor((1 - ratioHeight) * 50) + '%');
                     } else {
-                        ratioHeight = (buy / sell).toFixed(2);
+                        ratioHeight = (buy1 / sell1).toFixed(2);
                         watch.find('.indicator').find('.up').css('height', '0');
                         watch.find('.indicator').find('.down').css('height', Math.floor((1 - ratioHeight) * 50) + '%');
                     }
 
+                    // set 1min ration
                     var ratio = '1 : 1';
-                    if (buy > sell) {
-                        ratio = (buy / Math.max(sell, 0.00000001)).toFixed(2) + ' : 1';
-                    } else if (sell > buy) {
-                        ratio = '1 : ' + (sell / Math.max(buy, 0.00000001)).toFixed(2);
+                    if (buy1 > sell1) {
+                        ratio = '<span class="uk-text-success">' + (buy1 / Math.max(sell1, 0.00000001)).toFixed(2) + ' : 1</span>';
+                    } else if (sell1 > buy1) {
+                        ratio = '<span class="uk-text-danger">1 : ' + (sell1 / Math.max(buy1, 0.00000001)).toFixed(2);
                     }
+                    watch.find('.watch-buysell-1').html(ratio);
 
+                    // set 5min ration
+                    ratio = '1 : 1';
+                    if (buy5 > sell5) {
+                        ratio = '<span class="uk-text-success">' + (buy5 / Math.max(sell5, 0.00000001)).toFixed(2) + ' : 1</span>';
+                    } else if (sell5 > buy5) {
+                        ratio = '<span class="uk-text-danger">1 : ' + (sell5 / Math.max(buy5, 0.00000001)).toFixed(2);
+                    }
                     watch.find('.watch-buysell-5').html(ratio);
-                }
-            );
 
-            $.get(
-                'https://poloniex.com/public',
-                {command: 'returnTradeHistory', currencyPair: pair, start: time - 900, end: time},
-                function(data) {
-                    var buy = 0,
-                        sell = 0;
+                    // set 15 min ratio
+                    ratio = '1 : 1';
+                    if (buy15 > sell15) {
+                        ratio = '<span class="uk-text-success">' + (buy15 / Math.max(sell15, 0.00000001)).toFixed(2) + ' : 1</span>';
+                    } else if (sell15 > buy15) {
+                        ratio = '<span class="uk-text-danger">1 : ' + (sell15 / Math.max(buy15, 0.00000001)).toFixed(2);
+                    }
+                    watch.find('.watch-buysell-15').html(ratio);
 
-                    $.each(data, function (i, trade) {
-                        if (trade.type == 'buy') {
-                            buy = buy + parseFloat(trade.amount);
-                        } else if (trade.type == 'sell') {
-                            sell = sell + parseFloat(trade.amount);
+                    // set 30 min ratio
+                    ratio = '1 : 1';
+                    if (buy30 > sell30) {
+                        ratio = '<span class="uk-text-success">' + (buy30 / Math.max(sell30, 0.00000001)).toFixed(2) + ' : 1</span>';
+                    } else if (sell30 > buy30) {
+                        ratio = '<span class="uk-text-danger">1 : ' + (sell30 / Math.max(buy30, 0.00000001)).toFixed(2);
+                    }
+                    watch.find('.watch-buysell-30').html(ratio);
+
+                    // set 60 min ratio
+                    ratio = '1 : 1';
+                    if (buy60 > sell60) {
+                        ratio = '<span class="uk-text-success">' + (buy60 / Math.max(sell60, 0.00000001)).toFixed(2) + ' : 1</span>';
+                    } else if (sell60 > buy60) {
+                        ratio = '<span class="uk-text-danger">1 : ' + (sell60 / Math.max(buy60, 0.00000001)).toFixed(2);
+                    }
+                    watch.find('.watch-buysell-60').html(ratio);
+
+                    // trigger alerts
+                    if (pair in notifications && Push.Permission.has()) {
+                        if (pair in ratioHistory) {
+                            if (ratioHistory[pair]['ratio1'] != buy1 > sell1) {
+                                if ('n1' in notifications[pair] && notifications[pair]['n1']) {
+                                    Push.create('PoloTrend', {
+                                        body: buy1 > sell1 ? pair + ' goes up!' : pair + ' goes down!',
+                                        timeout: 5000,
+                                        icon: 'assets/img/push-icon.png',
+                                        onClick: function () {
+                                            window.focus();
+                                            this.close();
+                                        }
+                                    });
+                                }
+                            }
+                            if (ratioHistory[pair]['ratio5'] != buy5 > sell5) {
+                                if ('n5' in notifications[pair] && notifications[pair]['n5']) {
+                                    Push.create('PoloTrend', {
+                                        body: buy5 > sell5 ? pair + ' goes up!' : pair + ' goes down!',
+                                        timeout: 5000,
+                                        icon: 'assets/img/push-icon.png',
+                                        onClick: function () {
+                                            window.focus();
+                                            this.close();
+                                        }
+                                    });
+                                }
+                            }
+                            if (ratioHistory[pair]['ratio15'] != buy15 > sell15) {
+                                if ('n15' in notifications[pair] && notifications[pair]['n15']) {
+                                    Push.create('PoloTrend', {
+                                        body: buy15 > sell15 ? pair + ' goes up!' : pair + ' goes down!',
+                                        timeout: 5000,
+                                        icon: 'assets/img/push-icon.png',
+                                        onClick: function () {
+                                            window.focus();
+                                            this.close();
+                                        }
+                                    });
+                                }
+                            }
+                            if (ratioHistory[pair]['ratio30'] != buy30 > sell30) {
+                                if ('n30' in notifications[pair] && notifications[pair]['n30']) {
+                                    Push.create('PoloTrend', {
+                                        body: buy30 > sell30 ? pair + ' goes up!' : pair + ' goes down!',
+                                        timeout: 5000,
+                                        icon: 'assets/img/push-icon.png',
+                                        onClick: function () {
+                                            window.focus();
+                                            this.close();
+                                        }
+                                    });
+                                }
+                            }
+                            if (ratioHistory[pair]['ratio60'] != buy60 > sell60) {
+                                if ('n60' in notifications[pair] && notifications[pair]['n60']) {
+                                    Push.create('PoloTrend', {
+                                        body: buy60 > sell60 ? pair + ' goes up!' : pair + ' goes down!',
+                                        timeout: 5000,
+                                        icon: 'assets/img/push-icon.png',
+                                        onClick: function () {
+                                            window.focus();
+                                            this.close();
+                                        }
+                                    });
+                                }
+                            }
                         }
-                    });
-
-                    var ratio = '1 : 1';
-                    if (buy > sell) {
-                        ratio = (buy / Math.max(sell, 0.00000001)).toFixed(2) + ' : 1';
-                    } else if (sell > buy) {
-                        ratio = '1 : ' + (sell / Math.max(buy, 0.00000001)).toFixed(2);
                     }
 
-                    watch.find('.watch-buysell-15').html(ratio);
+                    ratioHistory[pair] = {};
+                    ratioHistory[pair]['ratio1'] = buy1 > sell1;
+                    ratioHistory[pair]['ratio5'] = buy5 > sell5;
+                    ratioHistory[pair]['ratio15'] = buy15 > sell15;
+                    ratioHistory[pair]['ratio30'] = buy30 > sell30;
+                    ratioHistory[pair]['ratio60'] = buy60 > sell60;
                 }
             );
         });
-    }, 5000);
+    }, interval);
 
-    // web socket
+    // update price ticker with websocket
     var wsuri = "wss://api.poloniex.com";
     var connection = new autobahn.Connection({
         url: wsuri,
@@ -132,8 +292,6 @@ $(function () {
         function tickerEvent (ticker) {
             var watch = $('.watch[data-pair="' + ticker[0] + '"]');
             watch.find('.watch-ticker').html(ticker[1]);
-            watch.find('.watch-lowask').html(ticker[2]);
-            watch.find('.watch-highbid').html(ticker[3]);
         }
 
         session.subscribe('ticker', tickerEvent);
